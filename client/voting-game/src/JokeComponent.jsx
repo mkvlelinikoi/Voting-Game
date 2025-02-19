af5046dc8a18b1d1e81ad6cb1f6124e03043af66
mkvlelinikoi <nika.amirkhanovi@gmail.com>
2025-02-20 01:02:29 +0400

    added little UI and Reaction numbers

--- client/voting-game/src/JokeComponent.jsx
+++ client/voting-game/src/JokeComponent.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react'
 function JokeComponent() {
     const [joke, setJoke] = useState({});
     const [jokeId, setJokeId] = useState(1);
-    const [totalJokes, setTotalJokes] = useState(0)
+    const [smileReaction, setSmileReaction] = useState({});
 
 
     //functions that shows the joke with id
@@ -24,16 +24,29 @@ function JokeComponent() {
         if (jokeId > 0) {
             showId();
         }
-    }, [jokeId]);
+    }, []);
 
 
 
 
     return (
         <>
-            <h1>{joke.question}</h1>
-            <h1>{joke.answer}</h1>
-            <button onClick={showId}>Next Joke</button>
+            <div className="wrapper">
+                <div className="container">
+                    <div className="joke_content">
+                        <p>{joke.question}</p>
+                        <p>{joke.answer}</p>
+                    </div>
+                    <div className="functional_content">
+                        <div className="reaction_content">
+                            <button className="reaction_button"><img className='icon' src="./src/assets/happy-face.png" alt="smile" /><p>{joke.votes?.[0]?.smileValue ?? 0}</p></button>
+                            <button className="reaction_button"><img className='icon' src="./src/assets/like.png" alt="like" /><p>{joke.votes?.[1]?.likeValue ?? 0}</p></button>
+                            <button className="reaction_button"><img className='icon ' src="./src/assets/heart.png" alt="love" /><p>{joke.votes?.[2]?.loveValue ?? 0}</p></button>
+                        </div>
+                        <button className='next_button' onClick={showId}>Next Joke</button>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
